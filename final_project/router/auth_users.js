5d15ec511ebe44f86e1021e135151bb548c67c8d
pythonisb <121789359+pythonisb@users.noreply.github.com>
2023-01-02 03:19:16 -0600

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,45 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{ 
+    let existing_users = users.filter((user) => user.username === username);
+    if (existing_users.length != 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+    let clear_users = users.filter((user => (user.username === username) && (username.password === password)));
+    if (clear_users.length != 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
-
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!password || !username) {
+      return res.status(404).json({message: "Error. Please check your username and/or password."})
+  }
+
+  if (authenticatedUser(username, password)) {
+      let token = jwt.sign(
+          {data: password},
+          "access",
+          {expiresIn: 60*60}
+      );
+
+      req.session.authorization = {
+          token, username
+      };
+      return res.status(200).send("User successfuly logged in. Welcome.");  
+  } else {
+      return res.status(208).send("Please check username or password");
+  }
 });
 
 // Add a book review
