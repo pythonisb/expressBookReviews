d7d42803b547c97ff1e21fb6f1fb9036188bb203
pythonisb <121789359+pythonisb@users.noreply.github.com>
2023-01-02 12:53:31 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,7 +38,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     let findAuthor = new Promise((resolve, reject) => {
-        var author = req.params.author.replaceAll("+", " ");
+        var author = req.params.author.replaceAll("+", " "); //Replace as space can not be used in URL
         var authors = Object.keys(books);
         authors.forEach(function(key) {
             if(books[key].author == author) {
@@ -68,7 +68,6 @@ public_users.get('/title/:title',function (req, res) {
 public_users.get('/review/:isbn',function (req, res) {
   const isbn = req.params.isbn; //retreive review through ISBN (similar to task 2)
   res.send(JSON.stringify(books[isbn].reviews))
- // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
